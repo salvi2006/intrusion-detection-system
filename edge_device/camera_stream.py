546db3fb07f9bbd4905183259e586d80d3321eb0
salvi2006 <salvi.nama06@gmail.com>
2026-04-01 10:56:33 +0530

    Optimize CameraStream for edge devices using background threading

--- edge_device/camera_stream.py
+++ edge_device/camera_stream.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import threading
 from logger import log
 from config import CAMERA_URL
 
@@ -7,30 +8,47 @@ class CameraStream:
     def __init__(self, url=CAMERA_URL):
         self.url = url
         self.cap = None
+        self.latest_frame = None
+        self.running = False
         self.connect()
 
     def connect(self):
         log.info(f"Connecting to camera stream at {self.url}...")
         self.cap = cv2.VideoCapture(self.url)
-        if not self.cap.isOpened():
+        if self.cap.isOpened():
+            self.running = True
+            self.thread = threading.Thread(target=self._update, args=())
+            self.thread.daemon = True
+            self.thread.start()
+        else:
             log.warning("Failed to connect to camera. Will retry.")
-            
+
+    def _update(self):
+        # Background thread continuously drains the buffer to keep the frame real-time!
+        while self.running:
+            if self.cap is not None and self.cap.isOpened():
+                ret, frame = self.cap.read()
+                if ret:
+                    self.latest_frame = frame
+                else:
+                    time.sleep(0.01)
+            else:
+                time.sleep(0.1)
+                
     def read_frame(self):
-        if self.cap is None or not self.cap.isOpened():
+        if not self.running or self.cap is None or not self.cap.isOpened():
             log.warning("Camera disconnected. Attempting to reconnect in 5 seconds...")
             time.sleep(5)
             self.connect()
             return False, None
             
-        ret, frame = self.cap.read()
-        if not ret:
-            log.warning("Failed to read frame. Stream might have dropped.")
-            self.cap.release()
+        if self.latest_frame is None:
             return False, None
             
-        return True, frame
+        return True, self.latest_frame.copy()
         
     def release(self):
+        self.running = False
         if self.cap is not None:
             self.cap.release()
             log.info("Camera stream released.")
